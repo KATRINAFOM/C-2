e21d6ef872c54d59b8f6f7408be19f6e9300c6b1
Katrina <fominakatrina05@gmail.com>
2023-09-21 18:24:37 +0600

    Add Example_task2

---
+++ grp.work/Program.cs
@@ -0,0 +1,41 @@
+﻿//Console.Write("Введите число a: ");
+// int a = int.Parse(Console.ReadLine());
+// Console.Write("Введите число b: ");
+// int b = int.Parse(Console.ReadLine());
+
+// if (a == (b *b ))
+// {
+//     Console.WriteLine("True");
+// }
+// else 
+// {
+//     Console.WriteLine("False");
+// }
+Console.Write("Введите номер дня недели: ");
+string num = Console.ReadLine();
+switch(num)
+{
+    case "1": 
+    Console.WriteLine("Понедельник");
+    break;
+    case "2": 
+    Console.WriteLine("Вторник");
+    break;
+    case "3": 
+    Console.WriteLine("Среда");
+    break;
+    case "4": 
+    Console.WriteLine("Четверг");
+    break;
+    case "5": 
+    Console.WriteLine("Пятница");
+    break;
+    case "6": 
+    Console.WriteLine("Суббота");
+    break;
+    case "7": 
+    Console.WriteLine("Воскресенье");
+    break;
+    default: Console.WriteLine("Нет такого дня недели");
+    break;
+}
