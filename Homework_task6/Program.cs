02ed572a0207a0e731098b849edd8a5f3aa038e7
Katrina <fominakatrina05@gmail.com>
2023-09-21 19:15:31 +0600

    Add Homework_task6

---
+++ Homework_task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int remDiv = number % 2;
+
+if(remDiv == 0)
+{
+    Console.Write("Четное");
+}
+else
+{
+    Console.Write("Нечетное");
+}
